712b38e4b27289028eec2f16d8672fe197370153
Madan Malhotra <starthecode@gmail.com>
2025-02-07 13:49:27 +0530

    api fix

--- src/app/api/user-lists/route.ts
+++ src/app/api/user-lists/route.ts
@@ -6,70 +6,70 @@ import { NextRequest, NextResponse } from 'next/server';
 const API_KEY = process.env.API_KEY; // Define your API key in the .env file
 
 //insert User login details
-export async function POST(request: NextRequest) {
-  const body = await request.json(); // Parse the JSON body
+// export async function POST(request: NextRequest) {
+//   const body = await request.json(); // Parse the JSON body
 
-  const authHeader = request.headers.get('authorization');
+//   const authHeader = request.headers.get('authorization');
 
-  if (!authHeader || authHeader !== `Bearer ${API_KEY}`) {
-    return NextResponse.json(
-      { success: false, message: 'Unauthorized: Invalid API Key' },
-      { status: 401 }
-    );
-  }
+//   if (!authHeader || authHeader !== `Bearer ${API_KEY}`) {
+//     return NextResponse.json(
+//       { success: false, message: 'Unauthorized: Invalid API Key' },
+//       { status: 401 }
+//     );
+//   }
 
-  try {
-    const result = await prisma.userList.create({
-      data: {
-        userEmail: body.userEmail,
-        userName: body.userName,
-      },
-    });
-    return NextResponse.json({ success: true, data: result });
-  } catch (error) {
-    console.error('Error posting users data:', error);
-    return NextResponse.json(
-      { success: false, error: 'Failed to post data' },
-      { status: 500 }
-    );
-  }
-}
+//   try {
+//     const result = await prisma.userList.create({
+//       data: {
+//         email: body.userEmail,
+//         userName: body.userName,
+//       },
+//     });
+//     return NextResponse.json({ success: true, data: result });
+//   } catch (error) {
+//     console.error('Error posting users data:', error);
+//     return NextResponse.json(
+//       { success: false, error: 'Failed to post data' },
+//       { status: 500 }
+//     );
+//   }
+// }
 
 //get unique user details
-export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const userEmail = searchParams.get('userEmail'); // Extract email from query params
+// export async function GET(request: NextRequest) {
+//   const { searchParams } = new URL(request.url);
+//   const userEmail = searchParams.get('userEmail'); // Extract email from query params
 
-  const authHeader = request.headers.get('authorization');
+//   const authHeader = request.headers.get('authorization');
 
-  if (!authHeader || authHeader !== `Bearer ${API_KEY}`) {
-    return NextResponse.json(
-      { success: false, message: 'Unauthorized: Invalid API Key' },
-      { status: 401 }
-    );
-  }
+//   if (!authHeader || authHeader !== `Bearer ${API_KEY}`) {
+//     return NextResponse.json(
+//       { success: false, message: 'Unauthorized: Invalid API Key' },
+//       { status: 401 }
+//     );
+//   }
 
-  if (!userEmail) {
-    return NextResponse.json(
-      { success: false, message: 'Missing userEmail query parameter' },
-      { status: 400 }
-    );
-  }
+//   if (!userEmail) {
+//     return NextResponse.json(
+//       { success: false, message: 'Missing userEmail query parameter' },
+//       { status: 400 }
+//     );
+//   }
 
-  try {
-    const result = await prisma.userList.findUnique({
-      where: { userEmail },
-    });
+//   try {
+//     const result = await prisma.userList.findUnique({
+//       where: { userEmail },
+//     });
 
-    return NextResponse.json({ success: true, data: result });
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-    return NextResponse.json(
-      { success: false, error: 'Failed to fetch user data' },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json({ success: true, data: result });
+//   } catch (error) {
+//     console.error('Error fetching user data:', error);
+//     return NextResponse.json(
+//       { success: false, error: 'Failed to fetch user data' },
+//       { status: 500 }
+//     );
+//   }
+// }
 
 //Update User Credit
 export async function PUT(request: NextRequest) {
